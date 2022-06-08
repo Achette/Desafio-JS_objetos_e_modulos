c0007405b436597488b8f688096f18f2fb735967
Achette <i.achette@live.com>
2022-06-08 17:06:27 -0300

    Finished project

--- js/orderService.js
+++ js/orderService.js
@@ -2,14 +2,14 @@ import * as shippingService from "./shippingService.js";
 
 export function total(order) {
   try {
-    let basicprice = order.basic;
-    if (basicprice == 0) {
+    let basicPrice = order.basic;
+    if (basicPrice == 0) {
       throw "Empty chart =\\";
     }
     let discount = order.discount;
-    const shipping = shippingService.shipment(basicprice);
-    const total = basicprice - basicprice * (discount / 100) + shipping;
-    return `Order code: ${order.code} <br> <strong>Total price: ${total.toFixed(2)}</strong>`;
+    const shipping = shippingService.shipment(basicPrice);
+    const total = basicPrice - basicPrice * (discount / 100) + shipping;
+    return `Order code: ${order.code} <br> <strong>Total price: $${total.toFixed(2)}</strong>`;
 
   } catch (e) {
     return e;
